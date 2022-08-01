6c09a5a7748b425b3b857918fe1900d2eda59467
mohamed1maghraby-div <mohamed1maghraby@yahoo.com>
2022-08-01 10:31:15 +0200

    general improvements

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app-error')
 
 @section('title', __('Not Found'))
 @section('code', '404')
@@ -11,7 +11,7 @@
             <div class="font-alt mb-30 titan-title-size-4">Error 404</div>
             <div class="font-alt">The requested URL was not found on this server.<br/>That is all we know.
             </div>
-            <div class="font-alt mt-30"><a class="btn btn-border-w btn-round" href="index.html">Back to home page</a></div>
+            <div class="font-alt mt-30"><a class="btn btn-border-w btn-round" href="{{ route('user.index') }}">Back to home page</a></div>
         </div>
         </div>
     </section>
